032bbe6201bb92fd061d6af77e84e70d4ce383cf
Jeff Zhou <zhou.zhefu@gmail.com>
2014-03-31 15:19:14 +0800

    This project should be run together with netty4-study
    add Client of Time Server.

---
+++ src/main/java/com/jeff/netty/client/App.java
@@ -0,0 +1,13 @@
+package com.jeff.netty.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        new TimeClient().startClient();
+    }
+}
---
+++ src/main/java/com/jeff/netty/client/TimeClient.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jeff.netty.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ *
+ * @author winnie
+ */
+public class TimeClient {
+    
+    public void startClient() throws Exception {
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        ChannelFuture f;
+        
+        Bootstrap b = new Bootstrap();
+        b.group(workerGroup)
+         .channel(NioSocketChannel.class)
+         .option(ChannelOption.SO_KEEPALIVE, true)
+         .handler(new ChannelInitializer<SocketChannel>() {
+
+            @Override
+            protected void initChannel(SocketChannel ch) throws Exception {
+                ch.pipeline().addLast(new TimeClientHandler());
+                System.out.println("Client channel initiated.");
+            }
+             
+         });
+        
+        f = b.connect("localhost", 8080).sync();
+        System.out.println("Port binded");
+        
+        
+        f.channel().closeFuture().sync();
+        workerGroup.shutdownGracefully();
+        System.out.println("Time client ended. Yeah.");
+    }
+    
+    
+    
+}
